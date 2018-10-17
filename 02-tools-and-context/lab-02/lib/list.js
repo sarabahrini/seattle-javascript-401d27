21e5ba29c7c5750670330c1192b95c453cedda35
Jane Smith <example@email.com>
2018-10-16 21:02:50 -0700

    set up files and wrote functions with test - reduce finction an filter is remaining

---
+++ 02-tools-and-context/lab-02/lib/list.js
@@ -0,0 +1,72 @@
+'use strict';
+
+class List {
+  constructor() {
+    this.length = 0;
+  }
+
+  push(value) {
+    //add value to the back of the list
+    // assign to an index
+    this[this.length] = value;
+    //modify the length
+    this.length++;
+    //return the length
+    return this.length;
+  }
+
+
+  map(callback) {
+    let newList = new List();
+    for (let i = 0; i < this.length; i++) {
+      newList.push(callback(this[i]));
+    }
+    return newList;
+  }
+
+  pop() {
+    //remove last index in list 
+    delete this[this.length];
+    //modify the length of the list
+    this.length--;
+    return this.length;
+  }
+
+
+  slice(start, end) {
+    //define a new list
+    let newList = new List();
+    if (start === undefined) {
+      start = 0;
+    }
+    if (end === undefined) {
+      end = this.length;
+    }
+    for (let i = start; i < end; i++) {
+      newList.push(this[i]);
+    }
+    return newList;
+  }
+
+
+  // filter(callback) {
+  //   let newList = new List();
+  //   for (let i = 0; i < this.length; i++) {
+  //     if (callback(this[i]) === true) {
+  //       newList.push(this[i]);
+  //     }
+  //     return newList;
+  //   }
+
+
+    //  }
+
+
+    // reduce(){
+
+
+
+    // }
+
+  }
+module.exports = List;
