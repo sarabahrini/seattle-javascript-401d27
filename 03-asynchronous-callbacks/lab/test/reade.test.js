cc3655b99f173fb1a8f0f4b6882debfdcc27b62f
Jane Smith <example@email.com>
2018-10-22 07:15:06 -0700

    done

--- 03-asynchronous-callbacks/lab/test/reade.test.js
+++ 03-asynchronous-callbacks/lab/test/reade.test.js
@@ -8,27 +8,61 @@ let path1 = `${__dirname}/../data/eenie.txt`;
 let path2 = `${__dirname}/../data/meenie.txt`;
 let path3 = `${__dirname}/../data/moe.txt`;
 
+
 let pathArray = [path1, path2, path3];
 
-
 describe('Test the reader for error', () => {
-
+  
   it('Should return the array of files in the same order that they were passed in.', (done) => {
-
+    
 
     getFiles(pathArray, (err, actual) => {
-
       if (err) {
         throw console.log(err);
       }
 
+      // console.log(actual)
       const expected = pathArray;
-      
-      expect(actual).toBe(expected);
+
+      expect(actual).toEqual(expected);
+      done();
+    });
+
+  });
+
+  it('Should return false if the returned array is empty.', (done) => {
+
+    let altPath = ['aadd'];
+
+    getFiles(altPath, (err, actual) => {
+      // if (err) {
+        // throw console.log(err);
+      // }
+
+
+      expect(err).not.toBeNull();
       done();
 
     });
 
   });
+
+  it('Should return falsy if files are returned in the wrong order.', (done) => {
+
+    let altPath = [path2, path3, path1];
+
+    getFiles(pathArray, (err, actual) => {
+      // if (err) {
+        // throw console.log(err);
+      // }
+
+      expect(err).toBeFalsy;
+      done();
+      
+
+    });
+  });
+
 });
+
 
