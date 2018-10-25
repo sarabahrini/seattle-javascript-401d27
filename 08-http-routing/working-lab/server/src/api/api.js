b41217e54d9b5e2a6b395d4bdaf16291e81899cd
Jane Smith <example@email.com>
2018-10-24 22:23:36 -0700

    server is up on PORT

--- 08-http-routing/working-lab/server/src/api/api.js
+++ 08-http-routing/working-lab/server/src/api/api.js
@@ -2,13 +2,7 @@
 
 const router = require('../lib/router.js');
 
-/**
- * GET Route (/)
- * Accepts an optional "name" query string parameter and says Hello
- * test with httpie:
- *     http http://localhost:8080
- *     http http://localhost:8080?name=John
- */
+
 router.get('/', (req,res) => {
   res.statusCode = 200;
   res.statusMessage = 'OK';
@@ -17,13 +11,15 @@ router.get('/', (req,res) => {
   res.end();
 });
 
-/**
- * POST Route (/data)
- * Accepts a JSON object and simply regurgitates it back to the browser
- * test with httpie:
- *     echo '{"title":"Go Home","content":"foobar"}' | http post http://localhost:8080/data
- */
+
 router.post('/data', (req,res) => {
+  res.statusCode = 200;
+  res.statusMessage = 'OK';
+  res.write( JSON.stringify(req.body));
+  res.end();
+});
+
+router.post('/api/v1/notes', (req,res) => {
   res.statusCode = 200;
   res.statusMessage = 'OK';
   res.write( JSON.stringify(req.body) );
