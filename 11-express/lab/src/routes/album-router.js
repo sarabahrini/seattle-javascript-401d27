85e6d001c81868177afaa20cac25ae0bcd31c1f7
Jane Smith <example@email.com>
2018-10-30 17:49:23 -0700

    made changes to make delete post and get work properly and added README

--- 11-express/lab/src/routes/album-router.js
+++ 11-express/lab/src/routes/album-router.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import Album from '../models/album.js';
 import {request} from 'https';
+import { read } from 'fs';
 const router = new express.Router();
 
 let sendJSON = (res,data) => {
@@ -25,7 +26,14 @@ router.get('/api/v1/albums', (req,res) => {
   if ( req.query.id ) {
     Album.findOne(req.query.id)
       .then( data => sendJSON(res,data) )
-      .catch( err => serverError(res,err) );
+      .catch( err => { 
+        res.statusCode = 404;
+        res.statusMessage = 'Not Found';
+        res.setHeader('Content-Type', 'application/json');
+        res.write( JSON.stringify(err) );
+        res.end();
+
+      });
   }
   else {
     Album.fetchAll()
@@ -39,7 +47,11 @@ router.delete('/api/v1/albums', (req,res) => {
     Album.deleteOne(req.query.id)
       .then( success => {
         let data = {id:req.query.id,deleted:success};
-        sendJSON(res,data);
+        res.statusCode = 204;
+        res.statusMessage = 'OK';
+        res.setHeader('Content-Type', 'application/json');
+        res.write( JSON.stringify(data) );
+        res.end();;
       })
       .catch(console.error);
   }
@@ -47,9 +59,18 @@ router.delete('/api/v1/albums', (req,res) => {
 
 router.post('/api/v1/albums', (req,res) => {
   console.log(req.body);
+  
+  if(Object.keys(req.body).length === 0){
+        res.statusCode = 400;
+        res.statusMessage = 'Bad Request';
+        res.setHeader('Content-Type', 'application/json');
+        res.write( JSON.stringify(error) );
+        res.end();
+  };
+
   let record = new Album (req.body);
   record.save()
-    .then(data => sendJSON(res,data))
+    .then(data => {sendJSON(res,data)})
     .catch(console.error);
 });
 
