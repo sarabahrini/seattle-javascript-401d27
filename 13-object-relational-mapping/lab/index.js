90e0b20aa70ec970fad3d7476072f11ef774fe56
Jane Smith <example@email.com>
2018-11-04 09:43:11 -0800

    server set up

--- 13-object-relational-mapping/lab/index.js
+++ 13-object-relational-mapping/lab/index.js
@@ -1,5 +1,17 @@
-PORT=3000
+'use strict';
+
+require('dotenv').config();
 
 require('babel-register');
+
+const mongoose = require('mongoose');
+
+const options ={
+  useNewUrlParser: true,
+  useCreatIndex:true,
+  useFindAndModify:false,
+};
+
+mongoose.connect(process.env.MONGODB_URL, options);
 
 require('./server.js').start(process.env.PORT);
