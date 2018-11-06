329d3416649875ceddaf3599e35cfc765adabfb8
Jane Smith <example@email.com>
2018-11-06 15:45:14 -0800

    added codes to rout and middleware in lab16 still has to be reviewed

--- 16-basic-authentication/lab - Class/src/auth/router.js
+++ 16-basic-authentication/lab - Class/src/auth/router.js
@@ -13,10 +13,14 @@ router.post('/signup', async (request, response) => {
   try {
 
     // create the user with posted info
+    const user = await User.create(request.body);
 
     // make a token unique to the user
+    const token = user.generateToken();
     
     // respond with the token
+    response.send(token);
+   
 
   } catch (error) {
 
