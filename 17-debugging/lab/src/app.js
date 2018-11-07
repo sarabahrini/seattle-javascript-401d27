c6a2f867a08de6bf5123f9873f133e0a7438f4ae
Jane Smith <example@email.com>
2018-11-06 18:26:37 -0800

    made debuger work

--- 17-debugging/lab/src/app.js
+++ 17-debugging/lab/src/app.js
@@ -4,7 +4,7 @@ import error from './middleware/error.js';
 import chickenRouter from './chicken/router.js';
 import coopRouter from './coop/router.js';
 
-const app = express;
+const app = express();
 
 app.use(chickenRouter);
 app.use(coopRouter);
