e416729174cdd8cd0482c634e41cd62bc62e5a8e
Jane Smith <example@email.com>
2018-11-07 20:25:41 -0800

    added console.logs and added .j to the requuired links in index.js

--- 01-node-ecosystem/lab/index.js
+++ 01-node-ecosystem/lab/index.js
@@ -1,7 +1,8 @@
 'use strict'
-const greet = require('./lib/greet');
-const arithmetic = require('./lib/arithmetic');
+const greet = require('./lib/greet.js');
+const arithmetic = require('./lib/arithmetic.js');
 
 
-console.log(greet('str'));
-// console.log(arithmetic());
+console.log(greet('string'));
+console.log(arithmetic.add(1, 2));
+console.log(arithmetic.sub(3, 2));
